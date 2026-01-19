8274abc63aac721727b11d81c651d4a954c1f894
Md Rayhan <mohammadrayhan796@gmail.com>
2026-01-19 15:13:11 +0600

    feat: remove bottom border class from all list items

--- Script/script.js
+++ Script/script.js
@@ -1,34 +1,34 @@
+const categoryContent = document.getElementById("categoryContent");
 
-
-const CatagoryContainer = document.getElementById("categoryContent");
 const loadCategories = () => {
     fetch("https://news-api-fs.vercel.app/api/categories")
-    .then((res) => res.json ())
+    .then((res) => res.json())
     .then((data) => {
-        console.log(data.categories
-);
-const catagoriesCont = data.categories;
-console.log(catagoriesCont);
-  shwoCategorys (catagoriesCont);
-
-    } )
-    .catch((err) => {
-        console.log(err)
+        const catagoriesCont = data.categories;
+        shwoCategorys(catagoriesCont);
     })
-}
-const shwoCategorys = (catagoriesCont) => {
-    catagoriesCont.forEach( cat => {
-   
-    categoryContent.innerHTML += `<li id="${cat.id}" class="hover:border-b-5 hover:border-red-950 cursor-pointer">${cat.title}</li>
-    `;
-   
-    
-});
- categoryContent.addEventListener('click' , (e) => {
-        if(e.target.localName === 'li'){
-    console.log(e.target)
+    .catch((err) => {
+        console.log(err);
+    });
+};
 
+const shwoCategorys = (catagoriesCont) => {
+    
+    catagoriesCont.forEach(cat => {
+        categoryContent.innerHTML += `<li id="${cat.id}" class="hover:border-b-5 hover:border-red-700 border-rose-700 cursor-pointer">${cat.title}</li>`;
+    });
+
+   
+    categoryContent.addEventListener('click', (e) => {
+        const allli = document.querySelectorAll('li');
+        allli.forEach(li => {
+            li.classList.remove('border-b-5')
+        })
+        if (e.target.localName === 'li') {
+            console.log(e.target);
+            e.target.classList.add('border-b-5');
         }
-    } )
-}
+    }); 
+};
+
 loadCategories();
